102ea30bf61ea1c2e36428c102b42a0bdfae7832
evgkollab <evgkol85@gmail.com>
2026-01-04 16:28:40 +0000

    test auto deploy

--- helpers/utils.py
+++ helpers/utils.py
@@ -7,7 +7,8 @@ from google.cloud import bigquery
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 def setup_driver():
